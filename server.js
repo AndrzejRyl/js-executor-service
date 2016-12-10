416acff23b80836af3702a033dd949ec7d2a9024
FleenMobile\Andrzej <fleenmobile@gmail.com>
2016-12-10 18:36:18 +0100

    Revert newest commit from server side as it crashed everything ;)

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 const http = require('http');
 const vm = require('vm');
-const fs = require('fs');
 
 const port = process.env.PORT || 8080;
 
 const server = http.createServer(function(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'text/json');
-
-    var track = createSandbox(['./node_modules/tracking/build/tracking.js',
-        './node_modules/tracking/build/data/eye.js',
-        './node_modules/tracking/build/data/face.js',
-        './node_modules/tracking/build/data/mouth.js'
-    ], {
-        navigator: {},
-        tracking: {},
-        window: {}
-    }).tracking;
     const sandbox = {
-        tracking: track
+        r: 'placeholder'
     };
 
     var body = [];
@@ -26,16 +15,16 @@ const server = http.createServer(function(req, res) {
         body.push(chunk);
     }).on('end', function() {
         try {
-            const json = Buffer.concat(body).toString();
-            console.log('Received: ' + json.substring(0, 200) + '...');
-            const obj = JSON.parse(json);
+            const json = JSON.parse(Buffer.concat(body).toString());
+            console.log('Received code: ' + json.code);
+            console.log('Received arguments: ' + json.args);
 
-            const vmCode = 'f = ' + obj.code + '; f(' + args(obj.args) + ');';
-            const result = JSON.stringify(vm.runInNewContext(vmCode, sandbox));
+            const vmCode = 'f = ' + json.code + '; r = f(' + args(json.args) + ');';
+            const result = vm.runInNewContext(vmCode, sandbox);
             console.log('Computed: ' + result);
 
             res.writeHead(200);
-            res.end(result);
+            res.end(sandbox.r.toString());
         } catch (e) {
             console.error("Error while handling request: ", e);
             res.writeHead(400);
@@ -47,29 +36,10 @@ const server = http.createServer(function(req, res) {
 console.log("Running on port: ", port);
 server.listen(port);
 
-function createSandbox(files, /*optional*/ sandbox) {
-    var source, script, result;
-    if (!(files instanceof Array)) {
-        files = [files];
-    }
-    source = files.map(function(file) {
-        return fs.readFileSync(file, 'utf8');
-    }).join('');
-    if (!sandbox) {
-        sandbox = {};
-    }
-    script = new vm.Script(source);
-    result = script.runInNewContext(sandbox);
-    return sandbox;
-};
-
 function args(a) {
     var arguments = '';
     for (i = 0; i < a.length; i++) {
-        if (typeof a[i] === 'string') arguments += '\'' + a[i] + '\''
-        else arguments += JSON.stringify(a[i]);
-
-        if (i < a.length - 1) arguments += ', ';
+        arguments += (typeof a[i] === 'string' ? '\'' + a[i] + '\'' : a[i]) + ', ';
     }
-    return arguments;
+    return arguments.substring(0, arguments.length - 2);
 }
