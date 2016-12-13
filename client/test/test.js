996d9672be757874c467f2a5d1f77bba50d12668
FleenMobile\Andrzej <fleenmobile@gmail.com>
2016-12-13 14:33:22 +0100

    Adjust test to new server communication
    I don't mock server anymore as it uses well designed queues. I just check whether client builds correct request for queue.

--- client/test/test.js
+++ client/test/test.js
@@ -1,18 +1,15 @@
 process.env.SERVER_HOSTNAME = "localhost";
-var assert = require('assert');
-var chai = require('chai');
-var should = chai.should;
-var expect = chai.expect;
-var assert = chai.assert;
-var http = require('http');
-var express	= require("express");
-var app	= express();
+const chai = require('chai');
+const http = require("http");
+const should = chai.should;
+const expect = chai.expect;
+const assert = chai.assert;
 
 // To test
-var fileUpload = require("../file_upload.js");
-var client = require("../client.js");
-var clientAPI = require("../client_api.js");
-var testFunction = require("./test_function.js");
+const fileUpload = require("../file_upload.js");
+const client = require("../client.js");
+const clientAPI = require("../client_api.js");
+const testFunction = require("./test_function.js");
 
 describe('Client', function() {
 	describe('File upload', function() {
@@ -36,48 +33,16 @@ describe('Client', function() {
 
 	describe('Contact with the server', function() {
 		
-		it('should send correct data to server and get the response', function(done) {
-    		this.timeout(15000);
-			setTimeout(done, 15000);
-
+		it('should send correct data to server', function() {
 			var args = ['foo', 'bar'];
+			var filePath = "./test/test_function.js";
+			var file = require("./test_function.js");
 
-			// Mock server
-			app.post('', function(req,res){
-    			var body = [];
-				req.on('data', function(chunk) {
-			        body.push(chunk);
-			    }).on('end', function() {
-			        try {
-			            const json = Buffer.concat(body).toString();
-			            const obj = JSON.parse(json);
-
-			            // Check if correct data was sent
-			            assert.equal(obj.args.length, args.length);
-			            assert.equal(obj.args[0], args[0]);
-			            assert.equal(obj.args[1], args[1]);
-			            assert.equal(obj.code, testFunction.function_to_calculate.toString());
-
-			            res.writeHead(200);
-			            res.end("foobar");
-			        } catch (e) {
-			            console.error("Error while handling request: ", e);
-			            res.writeHead(400);
-			            res.end("ERR");
-			        }
-			    });
-			});
-
-			app.listen(8080, function() {
-				console.log("Mock server on localhost:8080")
-			});
-
-			client.executeFunctionOnServer("./test/test_function.js", args, function(err, body) {
-				// Check if we got response
-				assert.isNull(err);
-				assert.equal(body, "foobar");
-		    	done();
-			});
+			var req = JSON.parse(client.getRequestBody(filePath, args));
+			assert.equal(req.args.length, args.length);
+			assert.equal(req.args[0], args[0]);
+			assert.equal(req.args[1], args[1]);
+			assert.equal(req.code, file.function_to_calculate.toString());
 		});
 
 	});
