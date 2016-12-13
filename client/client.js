1cba41694ba7e1ef5052f5ac63d386482ab84b07
FleenMobile\Andrzej <fleenmobile@gmail.com>
2016-12-13 13:38:45 +0100

    Add theoretical implementation of queues in client
    I have to wait for AWS to be activated.

--- client/client.js
+++ client/client.js
@@ -1,23 +1,62 @@
-var request = require('request');
-var api = require("./client_api.js");
-const hostname = process.env.SERVER_HOSTNAME || "192.168.99.100";
+const api = require("./client_api.js");
+const config = require('../config/config.json');
+const AWS = require('aws-sdk');
+var msgId;
 
 // Listen for posts/gets
 api.start();
 
+// Request queue
+AWS.config.loadFromPath('../config/aws_config.json');
+var requestsQueue = new AWS.SQS({
+    apiVersion: config.SQS_API_VERSION,
+    params: {
+        QueueUrl: config.REQUESTS_SQS_QUEUE_URL
+    }
+});
+
+// Response queue
+var responsesQueue = new AWS.SQS({
+    apiVersion: config.SQS_API_VERSION,
+    params: {
+        QueueUrl: config.RESPONSES_SQS_QUEUE_URL
+    }
+});
+
 module.exports.executeFunctionOnServer = function executeFunctionOnServer(filePath, arguments, callback) {
 	var file = require(filePath);
-	request({
-		url: "http://" + hostname + ":8080",
-		method: "POST",
-		body: JSON.stringify({
+	requestsQueue.sendMessage({
+        MessageBody: JSON.stringify({
 			code:file.function_to_calculate.toString(),
 			args: arguments})
-	}, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			callback(null, body);
-		} else {
-			callback("ERROR: " + error);
-		}
-	});
+    }, function(err, data) {
+    	callback(err);
+        msgId = data.MessageId;
+    });
+
+
+    setTimeout(function() {
+        // then
+        checkResponse(msgId, callback);
+    }, 5000);
+}
+
+function checkResponse(msgId, callback) {
+    responsesQueue.receiveMessage({
+        MessageAttributeNames: [
+            "All"
+        ]
+    }, function(err, data) {
+        var msg = data.Messages[0];
+        if (msg.MessageAttributes.requestId.StringValue == msgId) {
+            responsesQueue.deleteMessage({
+                ReceiptHandle: msg.ReceiptHandle
+            }, function(err, data) {
+                if (err) callback(err);
+                callback(null, msg);
+            });
+        } else {
+            checkResponse(msgId, callback);
+        }
+    });
 }
